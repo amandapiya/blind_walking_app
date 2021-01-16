219342c8980c45be35c23a6192e8331b237eab6c
Kristin <kristin.ng@tufts.edu>
2021-01-16 14:22:18 -0500

    v2. play sound in given mp3 file

--- blind_walking/ViewController.swift
+++ blind_walking/ViewController.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        var blindSound: AVAudioPlayer?
+        
+        //locate sound + create URL
+        let path = Bundle.main.path(forResource: "blind.mp3", ofType:nil)!
+        let url = URL(fileURLWithPath: path)
 
+        do {
+            blindSound = try AVAudioPlayer(contentsOf: url)
+            //play sound
+            blindSound?.play()
+        } catch {
+            print("could not load file.")
+        }
+    }
 
 }
 
