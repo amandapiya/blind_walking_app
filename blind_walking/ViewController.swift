732914dc3c3107544fea2d9b27e4847fef0a69e5
amandapiya <apiyapan@caltech.edu>
2021-01-16 13:57:22 -0600

    save before upgrading to IOS 14

--- blind_walking/ViewController.swift
+++ blind_walking/ViewController.swift
@@ -28,6 +28,11 @@ class ViewController: UIViewController, BambuserViewDelegate {
          super.viewDidLoad()
          bambuserView.orientation = UIApplication.shared.statusBarOrientation
          self.view.addSubview(bambuserView.view)
+        
+        broadcastButton.addTarget(self, action: #selector(ViewController.broadcast), for: UIControl.Event.touchUpInside)
+        broadcastButton.setTitle("Broadcast", for: UIControl.State.normal)
+        self.view.addSubview(broadcastButton)
+
          //After all settings to BambuserView has been done, add a call to startCapture in the viewDidLoad method.
          //bambuserView.startCapture()
      }
@@ -37,8 +42,17 @@ class ViewController: UIViewController, BambuserViewDelegate {
          var statusBarOffset : CGFloat = 0.0
          statusBarOffset = CGFloat(self.topLayoutGuide.length)
          bambuserView.previewFrame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: self.view.bounds.size.width, height: self.view.bounds.size.height - statusBarOffset)
+        broadcastButton.frame = CGRect(x: 0.0, y: 0.0 + statusBarOffset, width: 100.0, height: 50.0);
+
      }
      
+    @objc func broadcast() {
+        NSLog("Starting broadcast")
+        broadcastButton.setTitle("Connecting", for: UIControl.State.normal)
+        broadcastButton.removeTarget(nil, action: nil, for: UIControl.Event.touchUpInside)
+        broadcastButton.addTarget(bambuserView, action: #selector(bambuserView.stopBroadcasting), for: UIControl.Event.touchUpInside)
+        bambuserView.startBroadcasting()
+    }
 
 }
 
